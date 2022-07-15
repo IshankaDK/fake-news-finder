6bafac8b0351716d6cfb137fcb7fc34ff3e08991
IshankaDK <idkodippili@gmail.com>
2022-07-15 18:58:32 +0530

    remove linked files and set using text field

--- fake_news_detection.py
+++ fake_news_detection.py
@@ -1,35 +1,34 @@
-#Importing the libraries
-import pandas as pd
-import numpy as np
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd 
+import numpy as np 
 import pickle
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 
-#Importing the cleaned file containing the text and label
-news = pd.read_csv('news.csv')
-X = news['text']
-y = news['label']
+df = pd.read_csv('news.csv')
+df.columns = ['id','title','text','label']
+# print(df.shape)
+# print(df.head())
 
-#Splitting the data into train
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+labels = df.label
+X_train, X_test, y_train, y_test = train_test_split(df['text'],labels,test_size=1)
 
-#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Multinomial Naive Bayes model
-pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
-                    ('nbmodel', MultinomialNB())])
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 
-#Training our data
-pipeline.fit(X_train, y_train)
+# Fit and transform train set, transform test set
+tfidf_train = tfidf_vectorizer.fit_transform(X_train) 
+tfidf_test  = tfidf_vectorizer.transform(X_test)
 
-#Predicting the label for the test data
-pred = pipeline.predict(X_test)
+# Initialize a PassiveAggressiveClassifier
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
 
-#Checking the performance of our model
-print(classification_report(y_test, pred))
-print(confusion_matrix(y_test, pred))
+# saving vectorizer
+with open('tfid.pickle','wb') as f:
+    pickle.dump(tfidf_vectorizer,f)
 
-#Serialising the file
-with open('model.pickle', 'wb') as handle:
-    pickle.dump(pipeline, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# saving model
+with open('model_fakenews.pickle','wb') as f:
+    pickle.dump(pac,f)
--- main.py
+++ main.py
@@ -1,44 +1,26 @@
-#Importing the Libraries
-import numpy as np
-from flask import Flask, request,render_template
-from flask_cors import CORS
-import os
-# from sklearn.externals import joblib
+from flask import Flask, render_template, request, url_for, Markup, jsonify
 import pickle
-import flask
-import os
-import newspaper
-from newspaper import Article
-import urllib
-import nltk
 
-#Loading Flask and assigning the model variable
 app = Flask(__name__)
-CORS(app)
-app=flask.Flask(__name__,template_folder='templates')
-
-with open('model.pickle', 'rb') as handle:
-	model = pickle.load(handle)
+pickle_in = open('model_fakenews.pickle','rb')
+pac = pickle.load(pickle_in)
+tfid = open('tfid.pickle','rb')
+tfidf_vectorizer = pickle.load(tfid)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
+def home():
+ 	return render_template("index.html")
 
-@app.route('/predict',methods=['GET','POST'])
-def predict():
-    url =request.get_data(as_text=True)[5:]
-    url = urllib.parse.unquote(url)
-    nltk.download()
-    article = Article(str(url))
-    article.download()
-    article.parse()
-    article.nlp()
-    news = article.summary
-    # news = "dsafffffffffffhhlhjffffffhgutjiyrkiuyhmkdyjygyjyrjkyuhjklfuj,kjulf,uj,l"
-    # print(news)
-    pred = model.predict([news])
-    return render_template('index.html', prediction_text='The news is "{}"'.format(pred[0]))
-    
-if __name__=="__main__":
-    port=int(os.environ.get('PORT',5000))
-    app.run(port=port,debug=True,use_reloader=False)
+@app.route('/newscheck')
+def newscheck():	
+	abc = request.args.get('news')	
+	input_data = [abc.rstrip()]
+	# transforming input
+	tfidf_test = tfidf_vectorizer.transform(input_data)
+	# predicting the input
+	y_pred = pac.predict(tfidf_test)
+	return jsonify(result = y_pred[0])
+
+
+if __name__=='__main__':
+    app.run(debug=True)
